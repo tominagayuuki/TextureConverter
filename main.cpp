430925c2d0ae6a31f837edd7db20246b47c74520
tominagayuuki <k021g1421@g.neec.ac.jp>
2023-10-03 15:01:13 +0900

    02_03完了

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include<stdio.h>
 #include<cstdlib>
 
-int main() {
-	printf("Hello,World!\n");
+int main(int argc,char*argv[]) {
+
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		printf(argv[i]);
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
